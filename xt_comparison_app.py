520d8bc50eac76b7085b1b5c2b75bd12e7a34633
WTAnalysis <wt_analysis@outlook.com>
2025-11-27 14:38:24 +0000

    Update xt_comparison_app.py

--- xt_comparison_app.py
+++ xt_comparison_app.py
@@ -1846,7 +1846,7 @@ def main():
             facecolor='none',
             handlelength=5,
             edgecolor='None',
-            bbox_to_anchor=(.221, .94),
+            bbox_to_anchor=(.22175, .94),
             fontsize=8
         )
     
